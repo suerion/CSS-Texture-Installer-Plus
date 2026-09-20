9c91dbdd981071f94c4383aa5d29b9ce3f6c43c5
suerion <55131959+suerion@users.noreply.github.com>
2026-09-20 17:18:04 +0200

    fix: harden SteamCMD download and VPK extraction errors

--- util/steamcmd.js
+++ util/steamcmd.js
@@ -5,95 +5,155 @@ const axios = require('axios')
 const appDirectory = require('path').dirname(process.pkg ? process.execPath : (require.main ? require.main.filename : process.argv[0])).replace(/\\/g, '/')
 
 module.exports = {
-    installToPath: (path, callback) => {
-        return new Promise(async function (resolve) {
-			let chunks = 0
-            const c = await axios({
-                url: 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip',
-                method: 'GET',
-                responseType: 'stream'
-			})
-			const length = c.headers['content-length']
-            const writer = fs.createWriteStream(path)
-			c.data.on('data', (chunk) => {
-				chunks += chunk.length
-				callback({
-                    type: 'download',
-                    percent: Math.ceil(chunks / length * 100),
-					chunks: chunks
-				})
-            })
-            c.data.pipe(writer)
+    installToPath: async (path, callback) => {
+        let chunks = 0
 
-            writer.on('finish', () => {
-                seven.extractFull(path, `${appDirectory}/steam`, {
-                        $bin: appDirectory + '/7za.exe',
-                        $progress: true
-                    }).on('progress', (dat) => {
-                        callback({
-                            type: 'unzip',
-                            percent: Math.ceil(dat.percent),
-                            files: dat.fileCount
-                        })
-                    })
-                    .on('end', () => {
-                        resolve({
-                            path,
-                            success: true
-                        })
-
-                    })
-            })
-            writer.on('error', () => {
-                resolve(false)
-            })
-
+        const response = await axios({
+            url: 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip',
+            method: 'GET',
+            responseType: 'stream',
+            timeout: 30000,
+            validateStatus: status => status >= 200 && status < 300
         })
 
+        const length = Number(response.headers['content-length']) || 0
+        const writer = fs.createWriteStream(path)
+
+        response.data.on('data', (chunk) => {
+            chunks += chunk.length
+            callback({
+                type: 'download',
+                percent: length > 0 ? Math.ceil(chunks / length * 100) : 0,
+                chunks
+            })
+        })
+
+        await new Promise((resolve, reject) => {
+            response.data.on('error', reject)
+            writer.on('error', reject)
+            writer.on('finish', resolve)
+            response.data.pipe(writer)
+        })
+
+        await new Promise((resolve, reject) => {
+            seven.extractFull(path, `${appDirectory}/steam`, {
+                $bin: appDirectory + '/7za.exe',
+                $progress: true
+            })
+                .on('progress', (dat) => {
+                    callback({
+                        type: 'unzip',
+                        percent: Math.ceil(dat.percent),
+                        files: dat.fileCount
+                    })
+                })
+                .on('error', reject)
+                .on('end', resolve)
+        })
+
+        return {
+            path,
+            success: true
+        }
     },
+
     download: (appID, cmds, callback) => {
-        return new Promise(function (resolve) {
-            //Splits args into steamcmd valid ones
-            var args = cmds.concat('quit').map(function (x) {
+        return new Promise(function (resolve, reject) {
+            const args = cmds.concat('quit').map(function (x) {
                 return '+' + x
             }).join(' ').replace('{{app_id}}', appID).split(' ')
 
-            const c = pty.spawn(appDirectory + '/steam/steamcmd.exe', args, {
-                cwd: appDirectory + '/steam/'
-            })
-            c.on('data', (dat) => {
-                if (dat.includes('Update state')) {
-                    dat = dat.match(/\(([^)]+)\)/g)
-                    dat[0] = dat[0].replace(/[()]/g, '')
-                    dat[1] = dat[1].replace(/[()" "]/g, '').split('/').map(x => parseFloat(x))
-                    dat[1] = dat[1][0] / dat[1][1] * 100
-                    if (isNaN(dat[1])) dat[1] = 0
-                    var datObj = {
-                        code: dat[0],
-                        progress: dat[1]
+            let installed = false
+            let process
+
+            try {
+                process = pty.spawn(appDirectory + '/steam/steamcmd.exe', args, {
+                    cwd: appDirectory + '/steam/'
+                })
+            } catch (err) {
+                reject(err)
+                return
+            }
+
+            process.on('data', (output) => {
+                if (output.includes('Update state')) {
+                    const matches = output.match(/\(([^)]+)\)/g)
+
+                    if (matches && matches.length >= 2) {
+                        const code = matches[0].replace(/[()]/g, '')
+                        const progressParts = matches[1]
+                            .replace(/[()" "]/g, '')
+                            .split('/')
+                            .map(x => parseFloat(x))
+
+                        let progress = progressParts[0] / progressParts[1] * 100
+                        if (isNaN(progress)) progress = 0
+
+                        callback({
+                            code,
+                            progress
+                        })
                     }
-                    callback(datObj)
                 }
-                if (dat.includes(`App '${appID}' fully installed`)) {
-                    resolve()
+
+                if (output.includes(`App '${appID}' fully installed`)) {
+                    installed = true
                 }
-                //if (dat.indexOf('Success! App \'232330\' fully installed') !== -1) console.log(true)
+            })
+
+            process.on('exit', (event) => {
+                const exitCode = typeof event === 'number' ? event : event && event.exitCode
+
+                if (!installed) {
+                    reject(new Error(`SteamCMD exited before app ${appID} was fully installed${exitCode !== undefined ? ` (exit code ${exitCode})` : ''}.`))
+                    return
+                }
+
+                if (exitCode !== undefined && exitCode !== 0) {
+                    reject(new Error(`SteamCMD exited with code ${exitCode}.`))
+                    return
+                }
+
+                resolve()
             })
         })
     },
-    extract: ((file, callback) => {
-        return new Promise(function (resolve) {
-            if (!fs.existsSync(appDirectory + '/cssource/bin/vpk.exe')) return resolve(false)
-            const c = pty.spawn(appDirectory + '/cssource/bin/vpk.exe', [file])
-            c.on('data', (dat) => {
-                dat = dat.substr(dat.indexOf(" ") + 1)
+
+    extract: (file, callback) => {
+        return new Promise(function (resolve, reject) {
+            const vpkExecutable = appDirectory + '/cssource/bin/vpk.exe'
+
+            if (!fs.existsSync(vpkExecutable)) {
+                reject(new Error(`VPK tool could not be found: ${vpkExecutable}`))
+                return
+            }
+
+            let process
+
+            try {
+                process = pty.spawn(vpkExecutable, [file])
+            } catch (err) {
+                reject(err)
+                return
+            }
+
+            process.on('data', (output) => {
+                const fileName = output.substr(output.indexOf(' ') + 1)
                 callback({
-                    file: dat
+                    file: fileName
                 })
             })
-            c.on('exit', () => {
+
+            process.on('exit', (event) => {
+                const exitCode = typeof event === 'number' ? event : event && event.exitCode
+
+                if (exitCode !== undefined && exitCode !== 0) {
+                    reject(new Error(`vpk.exe exited with code ${exitCode}.`))
+                    return
+                }
+
                 resolve(true)
             })
         })
-    })
+    }
 }
