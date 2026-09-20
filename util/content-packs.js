d74ac767f5d08f1402c8f449d0ebea7091915f50
suerion <55131959+suerion@users.noreply.github.com>
2026-09-20 17:15:37 +0200

    refactor: add content pack definitions

---
+++ util/content-packs.js
@@ -0,0 +1,27 @@
+module.exports = {
+  css: {
+    id: 'css',
+    name: 'Counter-Strike: Source',
+    appId: '232330',
+    installDir: 'cssource',
+    gameDir: 'cstrike',
+    targetDir: 'css_content',
+    vpks: [
+      'cstrike_pak_dir.vpk'
+    ]
+  },
+  tf2: {
+    id: 'tf2',
+    name: 'Team Fortress 2',
+    appId: '232250',
+    installDir: 'tf2',
+    gameDir: 'tf',
+    targetDir: 'tf2_content',
+    vpks: [
+      'tf2_misc_dir.vpk',
+      'tf2_textures_dir.vpk',
+      'tf2_sound_misc_dir.vpk',
+      'tf2_sound_vo_english_dir.vpk'
+    ]
+  }
+}
